3c4935599ae15dfbfd6e7b597749e4a70354838d
Keito <cgerber35@outlook.com>
2025-09-10 16:37:42 +0200

    Ajout de contenu dans Donkey Kong Bananza

--- src/components/Game_Review/DonkeyKongBananzaComponent.vue
+++ src/components/Game_Review/DonkeyKongBananzaComponent.vue
@@ -11,7 +11,8 @@
     <div class="bordure-texte">
       <h2>Information</h2>
       <p>
-       ...
+        Développé par Nintendo EPD et édité par Nintendo.<br />
+        Sorti le 17 juillet 2025 sur Nintendo Switch 2, ce jeu marque le retour de Donkey Kong en 3D après 26 ans.  
       </p>
     </div>
   </section>
@@ -19,7 +20,7 @@
     <div class="bordure-texte">
       <h2>Histoire</h2>
       <p>
-        ...
+       Pris au piège dans un monde souterrain par la Void Company, Donkey Kong s’allie à Pauline pour tenter de regagner la surface.
       </p>
     </div>
   </section>
@@ -27,7 +28,16 @@
     <div class="bordure-texte">
       <h2>Mon ressentit</h2>
       <p>    
-           ...
+          Un excellent jeu qui mélange action, aventure et comédie.<br />
+          On explore 17 grands souterrains, chacun contenant ses propres zones secrètes. Chaque lieu est unique, riche en détails et rempli de surprises.<br />
+          Le gameplay est vraiment réussi, notamment grâce au fait qu’on peut détruire la plupart des éléments du décor.<br />
+          Le jeu propose 5 transformations, dont ma préférée : l’éléphant, capable d’aspirer tout sur son passage.<br />
+          Côté bande-son, on retrouve pas moins de 110 musiques, allant de thèmes épiques comme Heart of Gold aux morceaux liés aux transformations Bananza.<br />
+          Les animations sont super plaisantes à regarder et le jeu regorge de références qui feront sourire les fans de la série.  <br />
+          L’histoire réserve également de belles surprises que je vous laisse découvrir par vous-même.<br />
+          La complétion à 100% est agréable grâce à des listes claires qui permettent de suivre facilement ce qu’il reste à trouver.<br />
+          Au total, il y a 1000 bananes et 696 fossiles à collecter, ce qui m’a pris environ 65 heures de jeu. Les fossiles servent aussi à acheter des costumes, tous très bien réalisés, qui apportent des améliorations au personnage.<br />
+          On sent clairement la patte de l’équipe derrière l’excellent Super Mario Odyssey.
       </p>
     </div>
   </section>
